b84a3109a28d30e3afe1f463f4892bdefda731b4
DESKTOP-EIP30D5\zarevincom <zavikgamesone@gmail.com>
2021-01-16 11:44:40 +0300

    Added 2Eat development

---
+++ 2eat/src/components/ButtonBase.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<a href="" class="ButtonBase ButtonBase--gray"><slot></slot></a>
+
+<style lang="sass">
+  .ButtonBase
+    display: flex
+    text-align: center
+    min-width: 108px
+    max-width: 164px
+    height: 36px
+    padding: 6px 28px
+    border-radius: 12px
+    font-weight: 500
+
+    &--gray
+      background-color: #D8D8D8
+      color: #000
+</style>
