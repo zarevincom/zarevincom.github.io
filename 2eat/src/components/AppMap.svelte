ed2c0838c49f2bd5b8ec3620a76e3d85b0bb360d
DESKTOP-EIP30D5\zarevincom <zavikgamesone@gmail.com>
2021-01-16 12:06:25 +0300

    no message

--- 2eat/src/components/AppMap.svelte
+++
@@ -1,9 +0,0 @@
-<img src="/assets/imgs/map.png" alt="" class="map">
-
-<style lang="sass">
-    .map
-      display: block
-      margin: 0 auto
-      width: 100%
-      max-width: 1140px
-</style>
